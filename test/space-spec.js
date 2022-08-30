68fd7ce5b0434208e3e7dd84961a2a7d726fac3a
Christopher Cohen <103705214+cmcohen89@users.noreply.github.com>
2022-08-29 17:23:55 -0700

    Even more semicolons!

--- test/space-spec.js
+++ test/space-spec.js
@@ -25,8 +25,8 @@ describe('StellarObject', () => {
         expect(omegaCentauri.name).to.equal('Omega Centauri');
         expect(omegaCentauri.age).to.equal(11.5);
         expect(omegaCentauri.mass).to.equal(4);
-    })
-})
+    });
+});
 
 describe('Galaxy', () => {
 
@@ -34,20 +34,20 @@ describe('Galaxy', () => {
 
     beforeEach(() => {
         milkyWay = new Galaxy('Milky way', 13, 154000, 'spiral', []);
-    })
+    });
 
     it('should be a subclass of StellarObject', () => {
         expect(milkyWay instanceof StellarObject).to.be.true;
-    })
+    });
 
     it('should have a galaxy type', () => {
         expect(milkyWay.type).to.equal('spiral');
-    })
+    });
 
     it('should have an array of stars', () => {
         expect(milkyWay.stars).to.exist;
-    })
-})
+    });
+});
 
 describe('Star', () => {
 
@@ -60,23 +60,23 @@ describe('Star', () => {
         sol = new Star('Sol', 4.6, 1, milkyWay, []);
         earth = new Planet('Earth', 4.5, .0000003, true);
         // Note the mass numbers start being very inaccurate around this point, apologies to any space-lovers!
-    })
+    });
 
     it('should be a subclass of StellarObject', () => {
         expect(sol instanceof StellarObject).to.be.true;
-    })
+    });
 
     it('should have a galaxy it exists in', () => {
         expect(sol.galaxy).to.equal(milkyWay);
-    })
+    });
 
     it('should add itself to its galaxy\'s stars array upon instantiation', () => {
         expect(milkyWay.stars.includes(sol)).to.be.true;
-    })
+    });
 
     it('should have an array of planets', () => {
         expect(sol.planets).to.exist;
-    })
+    });
 
     describe('addPlanet()', () => {
         context('if instance of Planet class', () => {
@@ -85,8 +85,8 @@ describe('Star', () => {
                 sol.addPlanet(earth);
                 expect(sol.planets.includes(earth)).to.be.true;
                 expect(earth.star).to.equal(sol);
-            })
-        })
+            });
+        });
         context('if not instance of Planet class', () => {
             it('should throw an error', () => {
                 let moon = 'Moon';
@@ -96,9 +96,9 @@ describe('Star', () => {
                 } catch (error) {
                     expect(error.message).to.equal('Must be a planet');
                 }
-            })
-        })
-    })
+            });
+        });
+    });
 
     describe('printPlanets()', () => {
         context('if it has planets', () => {
@@ -109,16 +109,16 @@ describe('Star', () => {
                 sol.addPlanet(venus);
                 sol.addPlanet(earth);
                 expect(sol.printPlanets()).to.equal(`The planets orbiting Sol are Mercury and Venus and Earth`);
-            })
-        })
+            });
+        });
         context('if it does not have planets', () => {
             it('should print a string saying it is lonely', () => {
                 let betelgeuse = new Star('Betelgeuse', 0.1, 20, milkyWay, []);
                 expect(betelgeuse.printPlanets()).to.equal('I am a lonely star without any planets!');
-            })
-        })
-    })
-})
+            });
+        });
+    });
+});
 
 describe('Supergiant', () => {
 
@@ -128,11 +128,11 @@ describe('Supergiant', () => {
     beforeEach(() => {
         milkyWay = new Galaxy('Milky way', 13, 154000, 'spiral', []);
         betelgeuse = new Supergiant('Betelgeuse', 0.1, 20, milkyWay, []);
-    })
+    });
 
     it('should be a subclass of Star', () => {
         expect(betelgeuse instanceof Star).to.be.true;
-    })
+    });
 
     it('should be able to go supernova', () => {
         let planetA = 'planetA', planetB = 'planetB';
@@ -140,8 +140,8 @@ describe('Supergiant', () => {
         betelgeuse.supernova();
         expect(betelgeuse.supernova()).to.equal('Betelgeuse explodes in a cosmically dazzling show of light!');
         expect(betelgeuse.planets.length).to.equal(0);
-    })
-})
+    });
+});
 
 describe('Planet', () => {
 
@@ -151,26 +151,26 @@ describe('Planet', () => {
     beforeEach(() => {
         earth = new Planet('Earth', 4.5, .0000003, true);
         moon = new Moon('Moon', 4.45, .0123, earth);
-    })
+    });
 
     it('should be a subclass of StellarObject', () => {
         expect(earth instanceof StellarObject).to.be.true;
-    })
+    });
 
 
     it('should have a property indicating if the planet is habitable', () => {
         expect(earth.habitability).to.be.true;
-    })
+    });
 
     it('should have an array of moons', () => {
         expect(earth.moons).to.exist;
-    })
+    });
 
     it('should have an array of astronauts if the planet is habitable', () => {
         let mars = new Planet('Mars', 4.5, .00000004, false)
         expect(mars.astronauts).not.to.exist;
         expect(earth.astronauts).to.deep.equal([]);
-    })
+    });
 
     describe('recruitAstronaut()', () => {
         it('should recruit a new astronaut and return him/her from the function', () => {
@@ -178,8 +178,8 @@ describe('Planet', () => {
             let buzz = earth.recruitAstronaut('Buzz Aldrin', 36, 'USA', 'mechanical engineering');
             expect(earth.astronauts.length).to.equal(1);
             expect(buzz instanceof Astronaut).to.be.true;
-        })
-    })
+        });
+    });
 
     describe('buildSpaceship()', () => {
         context('if astronaut has been properly recruited', () => {
@@ -189,8 +189,8 @@ describe('Planet', () => {
                 earth.buildSpaceship(buzz, 'Icarus', 'USA');
                 expect(buzz.spaceship.shipName).to.equal('Icarus');
                 expect(buzz.spaceship.astronaut.name).to.equal('Buzz Aldrin');
-            })
-        })
+            });
+        });
         context('if astronaut has not been properly recruited', () => {
             it('should throw an error', () => {
                 let ivan = new Astronaut('Ivan Smirnoff', 42, 'USSR', 'drinking vodka');
@@ -200,10 +200,10 @@ describe('Planet', () => {
                 } catch (error) {
                     expect(error.message).to.equal('Cannot build spaceship for unauthorized astronaut');
                 }
-            })
-        })
-    })
-})
+            });
+        });
+    });
+});
 
 describe('Moon', () => {
     let earth;
@@ -212,19 +212,19 @@ describe('Moon', () => {
     beforeEach(() => {
         earth = new Planet('Earth', 4.5, .0000003, true);
         moon = new Moon('Moon', 4.45, .0123, earth);
-    })
+    });
 
     it('should be a subclass of StellarObject', () => {
         expect(moon instanceof StellarObject).to.be.true;
-    })
+    });
 
     it('should add itself to its planet\'s moons array upon instantiation', () => {
         expect(earth.moons.includes(moon)).to.be.true;
-    })
+    });
 
     it('should have a colonizationStatus property that defaults to false', () => {
         expect(moon.colonizationStatus).to.be.false;
-    })
+    });
 
     it('should contain an array of nations that have successfully sent astronauts to it', () => {
         let buzz = earth.recruitAstronaut('Buzz Aldrin', 36, 'USA', 'mechanical engineering');
@@ -234,8 +234,8 @@ describe('Moon', () => {
         buzz.colonizeMoon(moon);
         ivan.colonizeMoon(moon);
         expect(Moon.flagsPlanted).to.deep.equal(['USA', 'USSR']);
-    })
-})
+    });
+});
 
 describe('Spaceship', () => {
     let earth = new Planet('Earth', 4.5, .0000003, true);
@@ -245,7 +245,7 @@ describe('Spaceship', () => {
         let ship = new Spaceship('Titanic', 'Germany');
         expect(ship.astronaut).to.equal(null);
         expect(ship.lightspeedCapability).to.be.false;
-    })
+    });
 
     it('should detect alien technology if it has been flown to the moon', () => {
         let buzz = earth.recruitAstronaut('Buzz Aldrin', 36, 'USA', 'mechanical engineering');
@@ -253,8 +253,8 @@ describe('Spaceship', () => {
         buzz.colonizeMoon(moon);
         expect(buzz.spaceship.detectAlienTechnology()).to.equal(`Voyager has detected alien technology on the dark side of the moon!`);
         expect(buzz.spaceship.lightspeedCapability).to.be.true;
-    })
-})
+    });
+});
 
 describe('Astronaut', () => {
     let buzz;
@@ -265,33 +265,33 @@ describe('Astronaut', () => {
         earth = new Planet('Earth', 4.5, .0000003, true);
         moon = new Moon('Moon', 4.45, .0123, earth);
         buzz = earth.recruitAstronaut('Buzz Aldrin', 36, 'USA', 'mechanical engineering');
-    })
+    });
 
     it('should have name, age, nationality, and expertise properties', () => {
         expect(buzz.name).to.equal('Buzz Aldrin');
         expect(buzz.age).to.equal(36);
         expect(buzz.nationality).to.equal('USA');
         expect(buzz.expertise).to.equal('mechanical engineering')
-    })
+    });
 
     it('should have a spaceship property that defaults to null', () => {
         expect(buzz.spaceship).to.equal(null);
-    })
+    });
 
     it('should be able to start a colony on a nearby moon if they have a spaceship', () => {
         earth.buildSpaceship(buzz, 'Voyager', 'USA');
         expect(buzz.colonizeMoon(moon)).to.equal('Buzz Aldrin begins a lunar colony and is now a Moonman!');
         expect(buzz.nationality).to.equal('Moonman');
         expect(moon.colonizationStatus).to.be.true;
-    })
+    });
 
     describe('astronautLogs', () => {
         it('should keep track of every astronaut recruited and their nationality', () => {
             let buzz = earth.recruitAstronaut('Buzz Aldrin', 36, 'USA', 'mechanical engineering');
             let ivan = earth.recruitAstronaut('Ivan Smirnoff', 42, 'USSR', 'drinking vodka');
             expect(Astronaut.astronautLogs).to.deep.equal({ 'Buzz Aldrin': 'USA', 'Ivan Smirnoff': 'USSR' });
-        })
-    })
+        });
+    });
 
     describe('ventureForth()', () => {
         context('if their ship does not have lightspeed capability', () => {
@@ -305,15 +305,15 @@ describe('Astronaut', () => {
                     expect(error instanceof SpacetimeError).to.equal(true);
                     expect(error.message).to.equal("Impossible! At least without some extraterrestrial technology...");
                 }
-            })
-        })
+            });
+        });
         context('if their ship has lightspeed capability', () => {
             it('should boldly go where no one has gone before', () => {
                 earth.buildSpaceship(buzz, 'Voyager', 'USA');
                 buzz.colonizeMoon(moon);
                 buzz.spaceship.detectAlienTechnology();
                 expect(buzz.ventureForth()).to.equal('Buzz Aldrin is the first human to explore deep space!');
-            })
-        })
-    })
-})
+            });
+        });
+    });
+});
